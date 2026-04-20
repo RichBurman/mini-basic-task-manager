34912a8703fcfb302e9f23ed994ac795043778c3
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-04-20 14:24:10 +0100

    update task-form to input and add a new task.

--- src/app/components/task-form/task-form.ts
+++ src/app/components/task-form/task-form.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { TaskService } from '../../services/task';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-task-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-form.html',
   styleUrl: './task-form.css',
 })
 export class TaskForm {
+    title = '';
 
+  constructor(private taskService: TaskService) {}
+
+  addTask() {
+    if (!this.title.trim()) return;
+
+    this.taskService.addTask(this.title);
+    this.title = '';
+  }
 }
