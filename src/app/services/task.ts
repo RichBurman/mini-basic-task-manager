29e1ff96702f91c69788a0bab7afc4f4eef145ae
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-04-20 14:17:05 +0100

    Build Tasks Service, for tasks, filter, addTask, toggleTask, deleteTask, setFilter

--- src/app/services/task.ts
+++ src/app/services/task.ts
@@ -1,8 +1,49 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal, computed } from '@angular/core';
+import { Task } from '../models/task';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Task {
+export class TaskService {
+  tasks = signal<Task[]>([]);
+
+  filter = signal<'all' | 'active' | 'completed'>('all');
+
+  filteredTasks = computed(() => {
+    const tasks = this.tasks();
+    const filter = this.filter();
+
+    if (filter === 'active') return tasks.filter(t => !t.completed);
+    if (filter === 'completed') return tasks.filter(t => t.completed);
+    return tasks;
+  });
+
+  addTask(title:string) {
+    const newTask: Task = {
+      id: Date.now(),
+      title,
+      completed: false,
+      priority: 'medium',
+      createdAt: new Date()
+    };
+
+    this.tasks.update(tasks => [...tasks, newTask]);
+  }
+
+  toggleTask(id: number) {
+    this.tasks.update( tasks =>
+      tasks.map(t =>
+        t.id === id ? { ...t, completed: !t.completed } : t
+      )
+    );
+  }
+
+  deleteTask(id: number) {
+    this.tasks.update( tasks => tasks.filter(t => t.id !== id));
+  }
+
+  setFilter(filter: 'all' | 'filter' | 'completed') {
+    this.filter.set(filter);
+  }
   
 }
